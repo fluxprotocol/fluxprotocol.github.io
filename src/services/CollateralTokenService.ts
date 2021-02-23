e7110bd081a13f2a76ce316e97727af75e05fb10
Franklin Waller <fr.l.waller@gmail.com>
2021-02-23 12:51:42 +0100

    fix(collateralToken): Fix issue where token image was not showing

--- src/services/CollateralTokenService.ts
+++ src/services/CollateralTokenService.ts
@@ -81,6 +81,7 @@ export async function getCollateralTokenMetadata(collateralTokenId: string): Pro
         });
 
         return {
+            ...defaultMetadata,
             ...metadata,
             collateralTokenId,
         };
