0111c4701a1723507e6652c5a57266d2fc2572fe
jasperdg <jasperdegooijer@gmail.com>
2021-02-25 12:07:04 +0100

    added potential profit for buy orders to swap overview

--- src/containers/TokenSwapper/components/SwapOverview/SwapOverview.tsx
+++ src/containers/TokenSwapper/components/SwapOverview/SwapOverview.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import { DEFAULT_SLIPPAGE } from '../../../../config';
 import { SwapFormValues } from '../../../../services/SwapService';
 import trans from '../../../../translation/trans';
-
+import Big from "big.js";
 import s from './SwapOverview.module.scss';
 import mutateFormValues from './utils/overviewMutation';
 
@@ -13,6 +13,11 @@ interface SwapOverviewProps {
 export default function SwapOverview({formValues}: SwapOverviewProps): ReactElement {
     let formattedFormValues = mutateFormValues(formValues);
     const collateralToken = formValues.fromToken.isCollateralToken ? formValues.fromToken : formValues.toToken;
+    console.log(formValues)
+    const amountIn = new Big(formValues.amountIn);
+    const amountOut = new Big(formValues.amountOut);
+    const divisor = new Big("100");
+    const profitPercentage = formValues.amountIn !== "0" ? amountOut.minus(amountIn).div(amountIn).mul(divisor).round(2).toString() : "0";
 
     return (
         <div className={s['swap-overview']}>
@@ -37,6 +42,15 @@ export default function SwapOverview({formValues}: SwapOverviewProps): ReactElem
                 <span className={s['swap-overview__info-key']}>{trans('market.overview.maxSlippage')}</span>
                 <span className={s['swap-overview__info-value']}>{DEFAULT_SLIPPAGE}%</span>
             </div>
+          
+            {
+                formValues.type === "BUY" && (
+                    <div className={s['swap-overview__info-row']}>
+                        <span className={s['swap-overview__info-key']}>{trans('market.overview.maxPayout')}</span>
+                        <span className={s['swap-overview__info-value']}>{formValues.formattedAmountOut || "0"} {formValues.fromToken.tokenSymbol} (+{profitPercentage}%)</span>
+                    </div>
+                )
+            }
         </div>
     );
 }
